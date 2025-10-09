cd6a1a68e34dc068b131de98a9399f93488e1e9b
lenah23 <lenulstep@gmail.com>
2025-10-09 15:52:32 +0400

    added another pom example

--- src/step-definitions/validations-steps.js
+++ src/step-definitions/validations-steps.js
@@ -1,7 +1,19 @@
 import { Then } from "@wdio/cucumber-framework";
 const compareText = require("./utils/compare-text");
+const { page } = require("../po/pages/index");
 
-Then('Page title should {string} {string}', async function(shouldBeParameter, titleText){
+Then(
+  "Page title should {string} {string}",
+  async function (shouldBeParameter, titleText) {
     const pageTitle = await browser.getTitle();
     compareText(pageTitle, titleText, shouldBeParameter);
-})
+  }
+);
+
+Then(/^modal window should( not)? be displayed$/, async (param) => {
+  let assert = expect(page("doctors").addDoctorModal.rootEl);
+  if (param) {
+    assert = assert.not;
+  }
+  return assert.toBeDisplayed();
+});
